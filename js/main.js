7a0be3a752ac692c86ea683034f074e246588826
kukushca <tinikitos@gmail.com>
2026-05-12 19:59:50 +0700

    fixed index.html file

--- js/main.js
+++ js/main.js
@@ -1,43 +1,42 @@
-import '../css/style.css';
+window.addEventListener('DOMContentLoaded', () => {
 
-const editableElements = document.querySelectorAll('[contenteditable="true"]');
+    const editableElements = document.querySelectorAll('[contenteditable="true"]');
+    editableElements.forEach(el => {
+        const savedValue = localStorage.getItem(el.id);
+        if (savedValue) el.innerText = savedValue;
 
-editableElements.forEach(el => {
-
-  const savedValue = localStorage.getItem(el.id);
-  if (savedValue) el.innerText = savedValue;
-
-  el.addEventListener('input', () => {
-    localStorage.setItem(el.id, el.innerText);
-  });
-});
+        el.addEventListener('input', () => {
+            localStorage.setItem(el.id, el.innerText);
+        });
+    });
 
 
-const downloadBtn = document.getElementById('download-btn');
-if (downloadBtn) {
-  downloadBtn.addEventListener('click', () => {
-    window.print();
-  });
-}
+    const downloadBtn = document.getElementById('download-btn');
+    if (downloadBtn) {
+        downloadBtn.onclick = () => {
+            window.print();
+        };
+    }
 
 
-document.addEventListener('mousedown', (e) => {
-  const target = e.target.closest('.resume-container');
-  if (!target) return;
+    document.addEventListener('mousedown', (e) => {
+        const target = e.target.closest('.resume-container');
+        if (!target) return;
 
-  const circle = document.createElement('span');
-  const diameter = Math.max(target.clientWidth, target.clientHeight);
-  const radius = diameter / 2;
-  const rect = target.getBoundingClientRect();
+        const circle = document.createElement('span');
+        const diameter = Math.max(target.clientWidth, target.clientHeight);
+        const radius = diameter / 2;
+        const rect = target.getBoundingClientRect();
 
-  circle.style.width = circle.style.height = `${diameter}px`;
-  circle.style.left = `${e.clientX - rect.left - radius}px`;
-  circle.style.top = `${e.clientY - rect.top - radius}px`;
-  circle.classList.add('ripple');
+        circle.style.width = circle.style.height = `${diameter}px`;
+        circle.style.left = `${e.clientX - rect.left - radius}px`;
+        circle.style.top = `${e.clientY - rect.top - radius}px`;
+        circle.classList.add('ripple');
 
-  const oldRipple = target.querySelector('.ripple');
-  if (oldRipple) oldRipple.remove();
+        const oldRipple = target.querySelector('.ripple');
+        if (oldRipple) oldRipple.remove();
 
-  target.appendChild(circle);
-  setTimeout(() => circle.remove(), 600);
+        target.appendChild(circle);
+        setTimeout(() => circle.remove(), 600);
+    });
 });
